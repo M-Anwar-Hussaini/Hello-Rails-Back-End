5a19780b3a08da6ef64dded824f36a1e61184866
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-01-18 13:56:21 +0430

    Update rails configuration

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,11 @@ module HelloRailsBackEnd
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001' # Replace with the URL of your React app
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
